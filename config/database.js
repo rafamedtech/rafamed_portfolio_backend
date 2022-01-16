798615e48b9360f005e98a02426a3b14ac0ce3a3
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-01-16 13:26:24 -0800

    deploy set up started

--- config/database.js
+++ config/database.js
@@ -1,11 +1,42 @@
-const path = require('path');
+const path = require("path");
+const parse = require("pg-connection-string").parse;
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            connection: {
+              host: config.host,
+              port: config.port,
+              user: config.user,
+              password: config.password,
+            },
+            options: {
+              ssl: false,
+            },
+          },
+        },
+      },
+    };
+  }
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "sqlite",
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
