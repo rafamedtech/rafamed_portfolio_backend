3b24d5cbce76662d78bbece975ab22d6fa7ad672
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-01-18 13:00:47 -0800

    s3 bucket plugin updated

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,14 @@
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    return {
-      upload: {
-        provider: "aws-s3",
-        providerOptions: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
-          region: env("AWS_REGION"),
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+      region: env("AWS_REGION"),
 
-          params: {
-            Bucket: env("AWS_BUCKET"),
-          },
-        },
+      params: {
+        Bucket: env("AWS_BUCKET"),
       },
-    };
-  }
-
-  return {};
-};
+    },
+  },
+});
