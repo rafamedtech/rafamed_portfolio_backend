ec37f036df28afa5ccfd8b87326810e4d27ccc97
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-01-18 19:02:10 -0800

    S3 bugfixed

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,6 @@ module.exports = ({ env }) => ({
       accessKeyId: env("AWS_ACCESS_KEY_ID"),
       secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
       region: env("AWS_REGION"),
-
       params: {
         Bucket: env("AWS_BUCKET"),
       },
